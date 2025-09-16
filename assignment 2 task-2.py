98538dd266df83a1af38c08efc8853b8b48c21ca
VIVAAN SHARMA <vivaansharma.edu.com@gmail.com>
2025-09-16 13:37:10 +0530

    Add files via upload
    module 2 assignment  task 1 and task2 attached

---
+++ assignment 2 task-2.py
@@ -0,0 +1,5 @@
+total=0
+for number in range(1,101):
+    total=total+number
+print('the sum of numbers from  1 to 100 is :  ', total)
+
---
+++ assignment2 task-1.py
@@ -0,0 +1,5 @@
+a=int(input('enter a number: '))
+if a% 2==0:
+ print(a,'it is even number ')
+else:
+    print(a,'it is odd number ')
